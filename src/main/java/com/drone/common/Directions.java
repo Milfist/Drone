8c8404cdf6c5f6b297d69eb9b4dd25df8989169c
MilfisT <milfist@gmail.com>
2018-04-09 15:03:10 +0200

    [TEST] Coverage 100%

--- src/main/java/com/drone/common/Directions.java
+++ src/main/java/com/drone/common/Directions.java
@@ -2,16 +2,6 @@ package com.drone.common;
 
 public enum Directions {
 
-	UP("UP"), DOWN("DOWN"), RIGHT("RIGHT"), LEFT("LEFT");
-
-	private String value;
-
-	private Directions(String s) {
-        value = s;
-    }
-
-	public String getValue() {
-		return value;
-	}
+	UP, DOWN, RIGHT, LEFT;
 
 }
